f7887e246627202b37026c9ef90a6dda1ca3a38f
JORGE PUMAR DODSWORTH <c1811318@pindare.grad.inf.puc-rio.br>
2019-04-12 14:37:18 -0300

    consertei os erros principais, programa funcionando

--- INF1416-G1: TrabLab 1/src/DigitalSignatureExample.java
+++ INF1416-G1: TrabLab 1/src/DigitalSignatureExample.java
@@ -7,58 +7,53 @@ import signature.MySignature;
 // de assinatura digital
 public class DigitalSignatureExample {
 
-	public static void main (String[] args) throws Exception {
-		
-		// verifica args e recebe o texto plano
-		if (args.length !=1) {
-			System.err.println("Usage: java DigitalSignatureExample text");
-			System.exit(1);
-		}
-		byte[] plainText = args[0].getBytes("UTF8");
+    public static void main (String[] args) throws Exception {
 
-		// gera o par de chaves RSA
-		System.out.println( "\nStart generating RSA key" );
-		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-		keyGen.initialize(1024);
-		KeyPair key = keyGen.generateKeyPair();
-		System.out.println( "Finish generating RSA key" );
-		
-		// MySignature
-		MySignature sig = MySignature.getInstance("MD5WithRSA");
-		
-		
-		// define um objeto signature para utilizar MD5 e RSA
-		// e assina o texto plano com a chave privada,
-		// o provider utilizado tambem eh impresso
-		
-//		Signature sig = Signature.getInstance("MD5WithRSA");
-		sig.initSign(key.getPrivate());
-		sig.update(plainText);
-		byte[] signature = sig.sign();
-//		System.out.println( sig.getProvider().getInfo() );
-		System.out.println( "\nSignature:" );
+        // verifica args e recebe o texto plano
+        if (args.length !=1) {
+            System.err.println("Usage: java DigitalSignatureExample text");
+            System.exit(1);
+        }
 
-		// converte o signature para hexadecimal
-		StringBuffer buf = new StringBuffer();
-		for(int i = 0; i < signature.length; i++) {
-			String hex = Integer.toHexString(0x0100 + (signature[i] & 0x00FF)).substring(1);
-			buf.append((hex.length() < 2 ? "0" : "") + hex);
-		}
+        byte[] plainText = args[0].getBytes("UTF8");
 
-		// imprime o signature em hexadecimal
-		System.out.println( buf.toString() );
+        // gera o par de chaves RSA
+        System.out.println( "\nStart generating RSA key" );
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024);
+        KeyPair key = keyGen.generateKeyPair();
+        System.out.println( "Finish generating RSA key" );
 
-		//
-		// verifica a assinatura com a chave publica
-		System.out.println( "\nStart signature verification" );
-		sig.initVerify(key.getPublic());
-		sig.update(plainText);
-		try {
-			if (sig.verify(signature)) {
-				System.out.println( "Signature verified" );
-			} else System.out.println( "Signature failed" );
-		} catch (SignatureException se) {
-			System.out.println( "Singature failed" );
-		}
-	}
+        // define um objeto signature para utilizar MD5 e RSA
+        // e assina o texto plano com a chave privada,
+        // o provider utilizado tambem eh impresso
+        MySignature sig = MySignature.getInstance("MD5");
+        sig.initSign(key.getPrivate());
+        sig.update(plainText);
+        byte[] signature = sig.sign();
+        System.out.println( "\nSignature:" );
+
+        // converte o signature para hexadecimal
+        StringBuffer buf = new StringBuffer();
+        for(int i = 0; i < signature.length; i++) {
+            String hex = Integer.toHexString(0x0100 + (signature[i] & 0x00FF)).substring(1);
+            buf.append((hex.length() < 2 ? "0" : "") + hex);
+        }
+
+        // imprime o signature em hexadecimal
+        System.out.println( buf.toString() );
+
+        //
+        // verifica a assinatura com a chave publica
+        System.out.println( "\nStart signature verification" );
+        sig.initVerify(key.getPublic());
+        sig.update(plainText);
+        try {
+            if (sig.verify(signature)) {
+                System.out.println( "Signature verified" );
+            } else System.out.println( "Signature failed" );
+        } catch (SignatureException se) {
+            System.out.println( "Singature failed" );
+        }
+    }
 }
--- INF1416-G1: TrabLab 1/src/signature/MySignature.java
+++ INF1416-G1: TrabLab 1/src/signature/MySignature.java
@@ -5,98 +5,111 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Key;
 import java.security.SignatureException;
-import java.security.interfaces.DSAKey;
-import java.security.interfaces.DSAParams;
-import java.security.interfaces.DSAPrivateKey;
-import java.security.interfaces.DSAPublicKey;
 
 public class MySignature {
 
-	// Singleton variable
+    // Singleton variable
     private static MySignature data = null; 
-    
+
     // Signature variables
     private String algo;
-	private DSAPublicKey pub;
-	private DSAPrivateKey priv;
-	private MessageDigest md;
-	
+    private PublicKey pub;
+    private PrivateKey priv;
+    private MessageDigest md;
+
     // Singleton access method
     public static MySignature getInstance(String algo) throws NoSuchAlgorithmException  { 
         if (data == null) {
-        	data = new MySignature(); 
+            data = new MySignature(); 
         }
+
         data.algo = algo;
-		data.md = MessageDigest.getInstance(algo);
+	data.md = MessageDigest.getInstance(algo);
         return data; 
     }
     
     // MARK: Sign methods
-    
+
+    // DOC: Store private key
     public void initSign(PrivateKey privateKey) throws InvalidKeyException {
     	try {
-			priv = (DSAPrivateKey) privateKey;
-		} catch (ClassCastException cce) {
-			throw new InvalidKeyException("Wrong private key type");
-		}
+            priv = (PrivateKey) privateKey;
+        } catch (ClassCastException cce) {
+            throw new InvalidKeyException("Wrong private key type");
+        }
     }
-    
+
+    // DOC: Compute message digest from plain text
     public void update(byte[] bytes) throws SignatureException {
     	try {
-			md.update(bytes);
-		} catch (NullPointerException npe) {
-			throw new SignatureException("No SHA digest found");
-		}
+            md.update(bytes);
+        } catch (NullPointerException npe) {
+            throw new SignatureException("No SHA digest found");
+        }
     }
-    
+
+    // DOC: Sign the message digest
     public byte[] sign() throws SignatureException {
     	byte b[] = null;
-		try {
-			b = md.digest();
-		} catch (NullPointerException npe) {
-			throw new SignatureException("No SHA digest found");
-		}
-		return crypt(b, priv);
+
+        // get message digest
+        try {
+            b = md.digest();
+        } catch (NullPointerException npe) {
+            throw new SignatureException("No SHA digest found");
+        }
+
+        //sign
+        return crypt(b, priv);
     }
     
     // MARK: Verify methods
-    
+
+    // DOC: store public key
     public void initVerify(PublicKey publicKey) throws InvalidKeyException {
     	try {
-			pub = (DSAPublicKey) publicKey;
-		} catch (ClassCastException cce) {
-			throw new InvalidKeyException("Wrong public key type");
-		}
+            pub = (PublicKey) publicKey;
+        } catch (ClassCastException cce) {
+            throw new InvalidKeyException("Wrong public key type");
+        }
     }
-    
+
+    // DOC: 
     public boolean verify(byte[] sigBytes) throws SignatureException {
-		byte b[] = null;
-		try {
-			b = md.digest();
-		} catch (NullPointerException npe) {
-			throw new SignatureException("No SHA digest found");
-		}
-		byte sig[] = crypt(sigBytes, pub);
-		return MessageDigest.isEqual(sig, b);
+        byte b[] = null;
+
+        try {
+            b = md.digest();
+        } catch (NullPointerException npe) {
+            throw new SignatureException("No SHA digest found");
+        }
+
+        // ONGOING: this seems wrong, check slides to verify
+        byte sig[] = crypt(sigBytes, pub);
+        System.out.println(sig);
+        System.out.println(sigBytes);
+        return MessageDigest.isEqual(sig, b);
     }
     
     
     // MARK: Auxiliary Methods
     
-    private byte[] crypt(byte s[], DSAKey key) {
-		DSAParams p = key.getParams();
-		int rotValue = p.getP().intValue();
-		byte d[] = rot(s, (byte) rotValue);
-		return d;
-	}
+    private byte[] crypt(byte s[], Key key) {
+        int rotValue = key.hashCode();
+        byte d[] = rot(s, (byte) rotValue);
+        return d;
+    }
 
-	private byte[] rot(byte in[], byte rotValue) {
-		byte out[] = new byte[in.length];
-		for (int i = 0; i < in.length; i++) {
-			out[i] = (byte) (in[i] ^ rotValue);
-		}
-		return out;
-	}
+    private byte[] rot(byte in[], byte rotValue) {
+        byte out[] = new byte[in.length];
+
+        for (int i = 0; i < in.length; i++) {
+            out[i] = (byte) (in[i] ^ rotValue);
+        }
+
+        return out;
+    }
 
 }
